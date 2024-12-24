6cec832f5abb8b32fe6cca42aabdab882490036e
Vaibhav <vaibhavjoshi1710@gmail.com>
2024-12-25 01:05:49 +0530

    "final-commit"

---
+++ src/app/pages/movie/movie.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IMAGES_SIZES } from '../../constants/images-sizes';
+import { Movie, MovieCredits, MovieImages, MovieVideo } from '../../models/movie';
+import { MoviesService } from '../../services/movies.service';
+
+@Component({
+  selector: 'app-movie',
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.scss']
+})
+export class MovieComponent implements OnInit {
+
+  movie: Movie | null = null;
+  imagesSizes = IMAGES_SIZES
+  movieVideos: MovieVideo[] = [];
+  movieImages: MovieImages | null = null;
+  movieCredits: MovieCredits | null = null;
+  constructor(private route : ActivatedRoute , private moviesService:MoviesService) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(({id})=>{
+      this.getMovie(id);
+      this.getMovieVideos(id);
+      this.getMovieImages(id);
+      this.getMovieCredits(id);
+    })
+  }
+
+  getMovie(id :string){
+    this.moviesService.getMovie(id).subscribe(movieData => {
+      this.movie = movieData;
+    })
+  }
+
+  getMovieVideos(id: string) {
+    this.moviesService.getMovieVideos(id).subscribe((movieVideosData) => {
+      this.movieVideos = movieVideosData;
+    });
+  }
+
+  getMovieImages(id: string) {
+    this.moviesService.getMovieImages(id).subscribe((movieImagesData) => {
+      this.movieImages = movieImagesData;
+    });
+  }
+
+  getMovieCredits(id: string) {
+    this.moviesService.getMovieCredits(id).subscribe((movieCreditsData) => {
+      this.movieCredits = movieCreditsData;
+    });
+  }
+
+}
